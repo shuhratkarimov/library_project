fef48b7cb6c61bf7bef657458ad4caf76fb5c604
shuhrat <n395b421na@gmail.com>
2025-02-02 12:11:14 +0500

    added

--- Controller/file.upload.controller.js
+++ Controller/file.upload.controller.js
@@ -1,57 +1,62 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const CarsModel = require("../Schemas/cars.schema");
+const BooksModel = require("../Schemas/books.schema");
+const AuthorsModel = require("../Schemas/authors.schema");
 const BaseError = require("../Utils/base_error");
-
 
 const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Multer konfiguratsiyasi
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    const fileNewName = `${Date.now()}${fileExtension}`;
+    cb(null, fileNewName);
+  },
+});
+
+const upload = multer({ storage });
+
 async function fileUploader(req, res, next) {
-  try {
-    const storage = multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, uploadDir);
-      },
-      filename: (req, file, cb) => {
-        const fileExtension = path.extname(file.originalname);
-        const fileNewName = `${Date.now()}${fileExtension}`;
-        cb(null, fileNewName);
-      },
+  upload.single("picture")(req, res, async (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!req.file) {
+      return next(BaseError.BadRequest(400, "Fayl yuklanmadi!"));
+    }
+
+    const fileLink = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    const { modelType, id } = req.params;
+
+    let model;
+    if (modelType === "book") {
+      model = BooksModel;
+    } else if (modelType === "author") {
+      model = AuthorsModel;
+    } else {
+      return next(BaseError.BadRequest(400, "Noto‘g‘ri model turi!"));
+    }
+
+    const updatedItem = await model.findByIdAndUpdate(id, { img: fileLink }, { new: true });
+
+    if (!updatedItem) {
+      return res.status(404).json({ message: "Ma'lumot topilmadi!" });
+    }
+
+    res.status(201).json({
+      message: "Rasm muvaffaqiyatli yuklandi",
+      image: fileLink,
     });
-
-    const upload = multer({ storage: storage });
-
-    upload.array("pictures", 3)(req, res, async (err) => {
-      if (err) {
-        return next(err);
-      }
-      if (req.files.length !== 3) {
-        return next(BaseError.BadRequest(404, "Yuklanadigan rasmlar soni 3 ta bo'lishi kerak!"));
-      }
-
-      const fileLinks = req.files.map(
-        (file) => `${req.protocol}://${req.get("host")}/uploads/${file.filename}`
-      );
-
-      const id = req.params.carId;
-      const car = await CarsModel.findByIdAndUpdate(id, { img: fileLinks }, { new: true });
-
-      if (!car) {
-        return res.status(404).json({ message: "Avtomobil topilmadi!" });
-      }
-
-      res.status(201).json({
-        message: "Rasmlar muvaffaqiyatli yuklandi",
-        images: fileLinks,
-      });
-    });
-  } catch (error) {
-    next(error);
-  }
+  });
 }
 
 module.exports = fileUploader;
--- Router/file.upload.routes.js
+++ Router/file.upload.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const fileUploadRouter = Router();
-const {uploadBookImages, upload} = require("../Controller/file.upload.controller");
+const fileUploader = require("../Controller/file.upload.controller");
 const {verifyAccessToken} = require("../Middlewares/verify_token_middleware")
-fileUploadRouter.post("/upload_book_image/:bookId", [verifyAccessToken, upload], uploadBookImages);
+fileUploadRouter.post("/upload/:id", verifyAccessToken, fileUploader);
 
 module.exports = fileUploadRouter;
