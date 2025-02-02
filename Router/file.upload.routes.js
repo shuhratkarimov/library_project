fbc5182ed479b018a11c7d36ddd477a35cd75866
shuhrat <n395b421na@gmail.com>
2025-02-02 12:58:44 +0500

    added

--- Router/file.upload.routes.js
+++ Router/file.upload.routes.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 const fileUploadRouter = Router();
 const fileUploader = require("../Controller/file.upload.controller");
 const {verifyAccessToken} = require("../Middlewares/verify_token_middleware")
-fileUploadRouter.post("/upload/modelType/:id", verifyAccessToken, fileUploader);
+fileUploadRouter.post("/upload/:modelType/:id", verifyAccessToken, fileUploader);
 
 module.exports = fileUploadRouter;
