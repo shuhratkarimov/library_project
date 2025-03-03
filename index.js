aa25df1c57df7ab43cc0387292bb3a2b6f9c6d9f
shuhratkarimov <shuhratkarimov.dev@gmail.com>
2025-03-03 13:18:04 +0500

    added

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 app.use(express.urlencoded({ extended: true }));
